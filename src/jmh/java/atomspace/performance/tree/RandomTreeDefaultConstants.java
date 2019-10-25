036ed7e3e4331ad152cdf6a69d619aa30872fc52
Alexander Scherbatiy <stellarspot@gmail.com>
2019-10-25 16:30:05 +0300

    Remove JMH tests

--- src/jmh/java/atomspace/performance/tree/RandomTreeDefaultConstants.java
+++
@@ -1,9 +0,0 @@
-package atomspace.performance.tree;
-
-import atomspace.performance.PerformanceModelConfiguration;
-
-public interface RandomTreeDefaultConstants {
-
-    PerformanceModelConfiguration DEFAULT_CONFIG = new PerformanceModelConfiguration(5, 5, 5, false);
-    RandomTreeModelParameters DEFAULT_TREE_PARAMETERS = new RandomTreeModelParameters(5, 5, 2);
-}
